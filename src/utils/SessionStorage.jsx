5b6e2fd7daec5613f2ffd13c45752a36f34603db
d0bloon <rodrigoalejandro.nunez1@alumnos.ulagos.cl>
2024-12-04 15:59:19 -0300

    Prueba

--- src/utils/SessionStorage.jsx
+++ src/utils/SessionStorage.jsx
@@ -1,7 +1,7 @@
 export const saveCartToSessionStorage = (cart) => {
     sessionStorage.setItem('cart', JSON.stringify(cart));
 };
-
+//Esto es de prueba
 export const getCartFromSessionStorage = () => {
     const cart = sessionStorage.getItem('cart');
     return cart ? JSON.parse(cart) : [];
