8cada1d59b48b2d40532bc184a72a0ed5f8913ef
d0bloon <rodrigoalejandro.nunez1@alumnos.ulagos.cl>
2024-12-05 21:01:37 -0300

    Proyecto Final 05/12/2024 21:01 Descripcion: mejoras

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -5,7 +5,7 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 const NotFound = () => {
     return (
         <div className="not-found-container">
-            <img src='PageNotFound.gif' alt="Not Found" className="not-found-gif" />
+            <a href="/"><img src='PageNotFound.gif' alt="Not Found" className="not-found-gif" /></a>
             <a href="/" className="home-link"><FontAwesomeIcon icon={faArrowLeft}/>VOLVER A LA PÁGINA PRINCIPAL</a>
         </div>
     );
