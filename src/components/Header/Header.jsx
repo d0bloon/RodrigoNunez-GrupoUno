3e60eb47165a97a69c12d11c2b09d9bf42779054
d0bloon <rodrigoalejandro.nunez1@alumnos.ulagos.cl>
2024-12-10 01:05:49 -0300

    actualizacion

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,31 +1,39 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMagnifyingGlass, faBagShopping, faUser, } from '@fortawesome/free-solid-svg-icons';
-import {Link} from "react-router-dom";
+import { faMagnifyingGlass, faBagShopping, faUser } from '@fortawesome/free-solid-svg-icons';
+import { Link } from "react-router-dom";
+import useBag from "../../UseBag/UseBag";
 import './header.css';
 
-
 const Header = () => {
-    return(
+    const cart = useBag(state => state.cart);
+    const totalQuantity = cart.reduce((sum, item) => sum + item.quantity, 0);
+
+    return (
         <div>
             <header className="header">
                 <div className="search-bar">
-                        <FontAwesomeIcon  className="search-icon" icon={faMagnifyingGlass}/>
-                        <input type="text" placeholder="Buscar..."/>
+                    <FontAwesomeIcon className="search-icon" icon={faMagnifyingGlass} />
+                    <input type="text" placeholder="Buscar..." />
                 </div>
                 <div className="header-container">
                     <div className="header-content">
                         <a href="/">
-                            <img className="logo" src="/logozapstore.png" alt="logo"/>
+                            <img className="logo" src="/logozapstore.png" alt="logo" />
                         </a>
                     </div>
                 </div>
                 <div className="icon">
-                    <Link to='/user'><FontAwesomeIcon className="icon-user" icon={faUser}/></Link>
-                    <Link to='/bag'><FontAwesomeIcon className="icon-bag" icon={faBagShopping}/></Link>
+                    <Link to='/user'><FontAwesomeIcon className="icon-user" icon={faUser} /></Link>
+                    <Link to='/bag'>
+                        <div className="icon-bag-container">
+                            <FontAwesomeIcon className="icon-bag" icon={faBagShopping} />
+                            {totalQuantity > 0 && <span className="cart-count">{totalQuantity}</span>}
+                        </div>
+                    </Link>
                 </div>
             </header>
         </div>
-    )
+    );
 }
 
-export default Header
+export default Header;
